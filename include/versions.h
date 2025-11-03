8206c8dc8ef9d781e755994b0ae74bcc28d2041d
Andreas Breitschopp <a-breitschopp@ab-tools.com>
2025-11-03 09:40:53 +0100

    Adjusted to OFM changes.

--- include/versions.h
+++ include/versions.h
@@ -1,26 +1,26 @@
 #pragma once
 
-#define MAIN_Version "6fd9e65"
+#define MAIN_Version "d28dcb5"
 #define KNX_Version "2.2.1+1b646c1"
 #define MODULE_LogicModule_Version "3.7.2+5e30d8d"
 #define MODULE_LogicModule_Version_Major 3
 #define MODULE_LogicModule_Version_Minor 7
 #define MODULE_LogicModule_Version_Revision 2
 #define MODULE_LogicModule_ETS 55
-#define MODULE_Common_Version "1.5.0+b16af76"
+#define MODULE_Common_Version "1.5.0+8937d53"
 #define MODULE_Common_Version_Major 1
 #define MODULE_Common_Version_Minor 5
 #define MODULE_Common_Version_Revision 0
 #define MODULE_Common_ETS 21
-#define MODULE_BusPowerSupply_Version "0.1.0+d966efc"
+#define MODULE_BusPowerSupply_Version "0.1.0+1b9afa3"
 #define MODULE_BusPowerSupply_Version_Major 0
 #define MODULE_BusPowerSupply_Version_Minor 1
 #define MODULE_BusPowerSupply_Version_Revision 0
 #define MODULE_BusPowerSupply_ETS 1
-#define MODULE_FileTransferModule_Version "0.1.2+4be8e34"
+#define MODULE_FileTransferModule_Version "0.1.3+ad9b0da"
 #define MODULE_FileTransferModule_Version_Major 0
 #define MODULE_FileTransferModule_Version_Minor 1
-#define MODULE_FileTransferModule_Version_Revision 2
+#define MODULE_FileTransferModule_Version_Revision 3
 #define MODULE_FileTransferModule_ETS 1
 #define MODULE_FunctionBlocks_Version "0.6.0+7663283"
 #define MODULE_FunctionBlocks_Version_Major 0
